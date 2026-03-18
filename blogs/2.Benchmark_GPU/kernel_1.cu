3523308eab0f7a94d05533faa824dce7e0965745
Marek Kowalik <marek-jozef.kowalik@capgemini.com>
2026-03-18 13:08:45 +0100

    Initial blog2 setup.

---
+++ blogs/2.Benchmark_GPU/kernel_1.cu
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cuda_runtime.h>
+
+    
+int main() {
+    int count = 0;
+    if (cudaGetDeviceCount(&count) == cudaSuccess && count > 0)
+        printf("CUDA device available\n");
+    else
+        printf("No CUDA device!\n");
+
+    return 0;
+}
