00072a4793bf501bf747f18a33af8b4c1d4ea652
Marek Kowalik <marek-jozef.kowalik@capgemini.com>
2026-03-27 19:42:49 +0100

    Update routines.

---
+++ routines/check_cuda.cu
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+
+// Helper function to check CUDA errors
+int checkCuda(cudaError_t result, const char* msg) {
+
+    if (result != cudaSuccess) {
+        std::cerr << "CUDA error (" << msg << "): " << cudaGetErrorString(result) << "\n";
+        return 1; 
+    }
+    return 0;
+}
+
+
+int main() {
+
+    return 0;
+}
---
+++ routines/clear_console.cu
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+
+void clearConsole() {
+    std::cout << "\033[2J\033[1;1H";
+}
+
+
+int main() {
+    clearConsole();
+    
+    return 0;
+}
